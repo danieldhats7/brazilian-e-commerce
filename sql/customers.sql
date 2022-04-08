3691058e547c31ea1b61efb9492751db63d4bdcc
Carlos Felipe Saldarriaga Bejarano <46731424+felipesalda@users.noreply.github.com>
2022-04-08 10:05:08 -0500

    fixed: we don't have name nor lastname -> customer_unique_id

--- sql/customers.sql
+++ sql/customers.sql
@@ -2,10 +2,9 @@ DROP TABLE IF EXISTS customers;
 
 CREATE TABLE customers (
 id INT PRIMARY KEY  NOT NULL,
-name VARCHAR(50) NOT NULL,
-last_name VARCHAR(50) NOT NULL,
-zip_code_prefix SMALLINT NOT NULL,
-city VARCHAR(20) NOT NULL,
-state CHAR(2) NOT NULL);
+customer_unique_id VARCHAR(50) NOT NULL,
+customer_zip_code_prefix SMALLINT NOT NULL,
+customer_city VARCHAR(20) NOT NULL,
+customer_state CHAR(2) NOT NULL);
 
 ALTER TABLE customers ADD CONSTRAINT customers_zip_code_prefix_geolocation_zip_code_prefix FOREIGN KEY (zip_code_prefix) REFERENCES geolocation(zip_code_prefix);
