1201f37ee2a7c860ecd36accb288689bd4bdc8f7
Carlos Felipe Saldarriaga Bejarano <46731424+felipesalda@users.noreply.github.com>
2022-04-08 10:20:36 -0500

    also id is pregenerated at kaggle

--- sql/customers.sql
+++ sql/customers.sql
@@ -1,10 +1,11 @@
 DROP TABLE IF EXISTS customers;
 
 CREATE TABLE customers (
-id INT PRIMARY KEY  NOT NULL,
+id VARCHAR(50) NOT NULL,
 customer_unique_id VARCHAR(50) NOT NULL,
 customer_zip_code_prefix SMALLINT NOT NULL,
 customer_city VARCHAR(20) NOT NULL,
 customer_state CHAR(2) NOT NULL);
+
 
 ALTER TABLE customers ADD CONSTRAINT customers_zip_code_prefix_geolocation_zip_code_prefix FOREIGN KEY (zip_code_prefix) REFERENCES geolocation(zip_code_prefix);
