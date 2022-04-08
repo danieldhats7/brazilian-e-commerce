c4b02abf1d2e97db0326b0094f03ba514f57bc8a
Carlos Felipe Saldarriaga Bejarano <46731424+felipesalda@users.noreply.github.com>
2022-04-08 11:33:33 -0500

    Update order_items.sql

--- sql/order_items.sql
+++ sql/order_items.sql
@@ -1,12 +1,12 @@
 DROP TABLE IF EXISTS order_items;
 
 CREATE TABLE order_items (
-order_id INT NOT NULL,
-product_id INT NOT NULL,
-seller_id INT NOT NULL,
+order_id VARCHAR(32) NOT NULL,
+order_item_id INT NOT NULL,
+product_id VARCHAR(32) NOT NULL,
+seller_id VARCHAR(32) NOT NULL,
 shipping_limit_date TIMESTAMP NOT NULL,
 price REAL NOT NULL,
-quantity SMALLINT NOT NULL,
 freight_value REAL NOT NULL);
 
 ALTER TABLE order_items ADD CONSTRAINT order_items_order_id_orders_id FOREIGN KEY (order_id) REFERENCES orders(id);
