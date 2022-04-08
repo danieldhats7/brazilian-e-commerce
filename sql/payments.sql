3da132c399285cc44c1680a1e42fd0003b294a89
Carlos Felipe Saldarriaga Bejarano <46731424+felipesalda@users.noreply.github.com>
2022-04-08 11:38:43 -0500

    update categories to the version

--- sql/payments.sql
+++ sql/payments.sql
@@ -1,10 +1,10 @@
 DROP TABLE IF EXISTS payments;
 
 CREATE TABLE payments (
-order_id INT NOT NULL,
+order_id VARCHAR(32) NOT NULL,
 payment_sequential SMALLINT NOT NULL,
 payment_type VARCHAR(20) NOT NULL,
-installments SMALLINT NOT NULL,
+payment_installments SMALLINT NOT NULL,
 value REAL NOT NULL);
 
 ALTER TABLE payments ADD CONSTRAINT payments_order_id_orders_id FOREIGN KEY (order_id) REFERENCES orders(id);
