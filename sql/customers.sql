4e7ea8285cff7c4e373c5a1acf2435f2dd6b5b44
Carlos Felipe Saldarriaga Bejarano <46731424+felipesalda@users.noreply.github.com>
2022-04-08 10:55:10 -0500

    Update customers.sql

--- sql/customers.sql
+++ sql/customers.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS customers;
 CREATE TABLE customers (
 id VARCHAR(32) NOT NULL,
 customer_unique_id VARCHAR(32) NOT NULL,
-customer_zip_code_prefix SMALLINT NOT NULL,
+customer_zip_code_prefix INT NOT NULL,
 customer_city VARCHAR(32) NOT NULL,
 customer_state CHAR(2) NOT NULL);
 
