3c7c1a034bb316f7be67f5ba82afcc45c89d6981
Daniel Reyes <danieldhatsct@gmail.com>
2022-04-09 23:32:05 -0500

    add script to create tables on database

---
+++ script.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.sql import text
+from constants import SQL_DIR, TABLE_NAMES
+from cfg import DB_CONNSTR
+
+
+engine = create_engine(DB_CONNSTR)
+
+
+def create_tables():
+    """Create db tables"""
+    with engine.connect() as con:
+        for file in TABLE_NAMES:
+            with open(SQL_DIR / f"{file}.sql") as f:
+                query = text(f.read())
+
+            con.execute(f"DROP TABLE IF EXISTS {file}")
+            con.execute(query)
+
+
+if __name__ == "__main__":
+    create_tables()
