62b97d516062de53b435814b2bf00dd92f4d60ee
Daniel Reyes <danieldhatsct@gmail.com>
2022-04-12 23:27:04 -0500

    update adding a class to transform orders table

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from cfg import RAW_DATA_URL_DICT
-from extract import Extract_csv
+from extract import Extract_csv, Extract_enrich_order
 from load import Loader
 import pandas as pd
 from constants import TABLES_NAMES
 
 
 extract_dict = {
-    'customers': Extract_csv('customers', RAW_DATA_URL_DICT['customers']),
-    'geolocation': Extract_csv('geolocation', RAW_DATA_URL_DICT['geolocation']),
-    'order_items': Extract_csv('order_items', RAW_DATA_URL_DICT['order_items']),
-    'order_reviews': Extract_csv('order_reviews', RAW_DATA_URL_DICT['order_reviews']),
-    'orders': Extract_csv('orders', RAW_DATA_URL_DICT['orders']),
-    'payments': Extract_csv('payments', RAW_DATA_URL_DICT['payments']),
-    'products': Extract_csv('products', RAW_DATA_URL_DICT['products']),
-    'sellers': Extract_csv('sellers', RAW_DATA_URL_DICT['sellers'])
+    #'customers': Extract_csv('customers', RAW_DATA_URL_DICT['customers']),
+    #'geolocation': Extract_csv('geolocation', RAW_DATA_URL_DICT['geolocation']),
+    #'order_items': Extract_csv('order_items', RAW_DATA_URL_DICT['order_items']),
+    #'order_reviews': Extract_csv('order_reviews', RAW_DATA_URL_DICT['order_reviews']),
+    'orders': Extract_enrich_order('orders', RAW_DATA_URL_DICT['orders']),
+    # 'payments': Extract_csv('payments', RAW_DATA_URL_DICT['payments']),
+    # 'products': Extract_csv('products', RAW_DATA_URL_DICT['products']),
+    # 'sellers': Extract_csv('sellers', RAW_DATA_URL_DICT['sellers'])
 }
 
 
@@ -26,6 +26,8 @@ def extract_raws() -> list[str]:
     file_paths = dict()
     for name, extractor in extract_dict.items():
         file_path = extractor.extract()
+        df = pd.read_csv(file_path)
+        file_path = extractor.tranform(df)
         file_paths[name] = file_path
     return file_paths
 
@@ -36,12 +38,23 @@ def load():
     pass
 
 if __name__ == "__main__":
-    file_paths = extract_raws()
-    load_ = Loader()
 
-    for name in TABLES_NAMES:
-        path = f'data/{name}.csv'
-        df = pd.read_csv(path)
-        load_.load_table(name, df)
-    print('Done!')
+    file_paths = extract_raws()
+
+
+    for name, file_path in file_paths.items():
+        df = pd.read_csv(file_path)
+        print(file_path)
+
+        df.head()
+
+
+
+    # load_ = Loader()
+
+    # for name in TABLES_NAMES:
+    #     path = f'data/{name}.csv'
+    #     df = pd.read_csv(path)
+    #     load_.load_table(name, df)
+    # print('Done!')
 
