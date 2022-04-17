f7eb08b6d433fa560e13c8cf625dadff12612d8d
Felipe Saldarriaga <felipe.salda@hotmail.com>
2022-04-16 22:54:16 -0500

    sqlalchemy connection working + gitignore added

---
+++ local_settings.py
@@ -0,0 +1,6 @@
+postgresql = {'pguser':'iafayaoovlpjvx',
+              'pgpasswd':'29f15a3de259ee7c672c8e91b8b2e6e25f5c69f232bf074213de99c8d72856a3',
+              'pghost':'ec2-3-218-171-44.compute-1.amazonaws.com',
+              'pgport':'5432',
+              'pgdb':'d6on26uasn5bdm'
+              }
