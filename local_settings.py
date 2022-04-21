380631a540db8e0bde003fd1db792c5fd3e88d2e
Felipe Saldarriaga <felipe.salda@hotmail.com>
2022-04-21 17:39:16 -0500

    Server creeds

--- local_settings.py
+++ local_settings.py
@@ -1,3 +1,4 @@
+# Heroku Creds: deprecated
 # postgresql = {'pguser':'iafayaoovlpjvx',
 #               'pgpasswd':'29f15a3de259ee7c672c8e91b8b2e6e25f5c69f232bf074213de99c8d72856a3',
 #               'pghost':'ec2-3-218-171-44.compute-1.amazonaws.com',
@@ -5,6 +6,7 @@
 #               'pgdb':'d6on26uasn5bdm'
 #               }
 
+# AWS Creeds
 postgresql = {'pguser':'data_engineer_team',
               'pgpasswd':'b^wvUOmJ%#VN',
               'pghost':'aws-e-commerce-db.cfcjuouheefl.us-east-1.rds.amazonaws.com',
