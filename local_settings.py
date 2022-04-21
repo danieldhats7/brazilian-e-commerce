14e72922bf1011e83fcceb296a86a4e5d370be2f
Felipe Saldarriaga <felipe.salda@hotmail.com>
2022-04-21 17:38:21 -0500

    Analysis completed with AWS database queries + req

--- local_settings.py
+++ local_settings.py
@@ -1,6 +1,13 @@
-postgresql = {'pguser':'iafayaoovlpjvx',
-              'pgpasswd':'29f15a3de259ee7c672c8e91b8b2e6e25f5c69f232bf074213de99c8d72856a3',
-              'pghost':'ec2-3-218-171-44.compute-1.amazonaws.com',
+# postgresql = {'pguser':'iafayaoovlpjvx',
+#               'pgpasswd':'29f15a3de259ee7c672c8e91b8b2e6e25f5c69f232bf074213de99c8d72856a3',
+#               'pghost':'ec2-3-218-171-44.compute-1.amazonaws.com',
+#               'pgport':'5432',
+#               'pgdb':'d6on26uasn5bdm'
+#               }
+
+postgresql = {'pguser':'data_engineer_team',
+              'pgpasswd':'b^wvUOmJ%#VN',
+              'pghost':'aws-e-commerce-db.cfcjuouheefl.us-east-1.rds.amazonaws.com',
               'pgport':'5432',
-              'pgdb':'d6on26uasn5bdm'
+              'pgdb':'postgres'
               }
